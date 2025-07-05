7e475d753edad82ff3e6a5aae4bcc891ed1be684
fantasy-01 <edg77777@foxmail.com>
2025-07-05 22:54:43 +0800

    更新了25.7.5学习的基础命令.

---
+++ concepts.py
@@ -0,0 +1,9 @@
+##transcript:转录本，DNA 被转录出来的一段 RNA（可以是 mRNA、lncRNA 等），携带遗传信息，为翻译成蛋白质做准备（或调控功能）。
+
+##CDS：编码区。既编码蛋白，也在成熟转录本中。
+##EXON：外显子。有的编码蛋白质(CDS)，有的不编码蛋白质。是在转录后会保留在成熟 mRNA 中的片段。
+##Intron为内含子。既不编码蛋白，也不在transcript中。
+##UTR：非编码区。不编码蛋白，
+
+#转录 后变成 pre-mRNA：包含所有 exon + intron。
+#剪接 后只保留 exon：形成成熟 mRNA（也就是 transcript）。
--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-# 这是一个示例 Python 脚本。
+##Ctrl + K快捷键进行提交。
+####25.7.5.完成了基本的配置并建立本地-远程ssh连接，将来的笔记都将在github保持同步。
 
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
+####   grep "^A" genome.fa  显示所有带A的行
+####   grep -c "^A" genome.fa 计数这样的行有多少
 
+####  awk '$3 == "gene"' genes.gtf > genes-only.gtf
+####  用 awk 来筛选 GTF 文件中 type 是 "gene" 的注释行，并把它们保存为一个新文件 genes-only.gtf。
+#  因为一个 GTF 文件每行是chr1  HAVANA  exon  11869 12227 . + . gene_id "ENSG..."; transcript_id "ENST...";
+#  其中第 3 列 $3 表示注释类型，gene是基因本体。执行后生成的文件实则只含基因。不含 transcript、exon、CDS 等内容。
+#  使用grep -c "gene" genes-only.gtf来计数有多少基因存在。
 
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
+#### sort -k2,2 genome_data.txt>sorted_data.txt
+#### 按第二列排序并将其输出到新文件中。
+##gene1  chr3-----gene2  chr1
+##gene2  chr1     gene3  chr2
+##gene3  chr2     gene1  chr3
 
+####   cut -f1 file.txt
+####   提取第一列。默认分隔是制表符Tab
+####   cut -d',' -f2 file.csv
+####   用冒号分隔提取第二列。
 
-# 按装订区域中的绿色按钮以运行脚本。
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+####   cut -f1 genomic.gff | sort | uniq | wc -l
+####   具有实用意义的组合拳。cut -f1提取第一列，一般是chr名称；sort进行排序；uniq去重复，最后按行数数。
